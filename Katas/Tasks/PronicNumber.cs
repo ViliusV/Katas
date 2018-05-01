61260f5e751fa537988841fd2d766f9474dcaccc
vilius.vaicekauskas <vilius.vaicekauskas@outlook.com>
2018-05-01 13:07:45 +0300

    Pronic number

---
+++ Katas/Tasks/PronicNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Katas.Tasks
+{
+	public class Kata
+	{
+		public static bool IsPronic(int n)
+		{
+			if (n < 0)
+			{
+				return false;
+			}
+
+			var root = Math.Floor(Math.Sqrt(n));
+			return root * (root + 1) == n;
+		}
+	}
+
+	[TestFixture]
+	public class Test
+	{
+		[Test]
+		public static void FixedTest()
+		{
+			Assert.AreEqual(true, Kata.IsPronic(0));
+			Assert.AreEqual(false, Kata.IsPronic(1));
+			Assert.AreEqual(true, Kata.IsPronic(2));
+			Assert.AreEqual(false, Kata.IsPronic(3));
+			Assert.AreEqual(false, Kata.IsPronic(4));
+			Assert.AreEqual(false, Kata.IsPronic(5));
+			Assert.AreEqual(true, Kata.IsPronic(6));
+			Assert.AreEqual(false, Kata.IsPronic(-3));
+			Assert.AreEqual(false, Kata.IsPronic(-27));
+		}
+	}
+}
