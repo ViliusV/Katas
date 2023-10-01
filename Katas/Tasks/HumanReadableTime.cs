99d32a2380efbc8cfdc06859ed80582f734c9d36
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2023-10-01 23:14:56 +0300

    Human readable time kata

---
+++ Katas/Tasks/HumanReadableTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.Tasks
+{
+	class HumanReadableTime
+	{
+		public static string GetReadableTime(int seconds)
+		{
+			var hours = seconds / 3600;
+			var remainingSeconds = seconds % 3600;
+
+			var minutes = remainingSeconds / 60;
+			remainingSeconds = remainingSeconds % 60;
+
+			var time = $"{hours:D2}:{minutes:D2}:{remainingSeconds:D2}";
+			return time;
+		}
+	}
+}
